c22f3a959d9a0a34463ba44ca704992869d38636
dynamicsaksham <154317798+dynamicsaksham@users.noreply.github.com>
2024-02-22 18:48:09 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('speechToText', (data) => {
+        io.emit('speechToText', data); // Broadcast speech text to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
